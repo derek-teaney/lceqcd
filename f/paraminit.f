6ff6284de395db96e423e2b1ee072ed0fa9287be
Derek Teaney <derek.teaney@stonybrook.edu>
2013-06-03 11:45:01 -0400

    Got rid of extra space

--- f/paraminit.f
+++ f/paraminit.f
@@ -21,7 +21,7 @@
       USE mpi
       implicit none
 *
-*including all global MESSAGE PASSING variables 
+*including all global MESSAGE PASSING variables
 *
       INCLUDE 'parallel_parameter.inc'
       INCLUDE 'parallel_mpi_types.inc'
@@ -56,13 +56,13 @@
 ******************************************************************************
 *
 *      nx/ny/nz_lattice    ! is the global lattice size
-*      dimproc             ! numbers of processes in every dimension mu = 1-4  
-*      nproc               ! total numbers of processes                        
-*      init                ! controls initialisation of gauge fields           
+*      dimproc             ! numbers of processes in every dimension mu = 1-4
+*      nproc               ! total numbers of processes
+*      init                ! controls initialisation of gauge fields
 *      rand                ! old(=1) or new(=0) random numbers
 *      save                ! save on disc yes or no (1,0)
-*      confstart           ! starting configuration                            
-*      beta                ! gauge coupling                
+*      confstart           ! starting configuration
+*      beta                ! gauge coupling
 *      x                   ! continuum parameter1
 *      y                   ! continuum parameter2
 *      lambda              ! overrelaxation parameter
@@ -72,57 +72,57 @@
 *      ntime               ! maximum of time for the program
 *      nsweep              ! number of trajectories
 *      imeas               ! number of sweeps after which to measure things
-*      imode               ! heatbath mode    
+*      imode               ! heatbath mode
 *******************************************************************************
          input_unit=1
          inputfile='inputfile'
-         
+
          open(Unit=input_unit,file=inputfile,status='unknown')
 
          read(input_unit,*,err=20) n_lattice(1), n_lattice(2), n_lattice(3)
-         read(input_unit,*,err=20) dimproc(1),   dimproc(2),   dimproc(3)   
+         read(input_unit,*,err=20) dimproc(1),   dimproc(2),   dimproc(3)
          read(input_unit,*,err=20) nproc
          read(input_unit,*,err=20) init
          read(input_unit,*,err=20) rand
          read(input_unit,*,err=20) save
          read(input_unit,*,err=20) confstart
-         read(input_unit,*,err=20) beta    
-         read(input_unit,*,err=20) x    
-         read(input_unit,*,err=20) y    
+         read(input_unit,*,err=20) beta
+         read(input_unit,*,err=20) x
+         read(input_unit,*,err=20) y
          read(input_unit,*,err=20) lambda
          read(input_unit,*,err=20) alpha
          read(input_unit,*,err=20) limit_or
          read(input_unit,*,err=20) limit_it
          read(input_unit,*,err=20) inorm
-         read(input_unit,*,err=20) nsweep_flag   
-         read(input_unit,*,err=20) ntime   
-         read(input_unit,*,err=20) nsweep   
+         read(input_unit,*,err=20) nsweep_flag
+         read(input_unit,*,err=20) ntime
+         read(input_unit,*,err=20) nsweep
          read(input_unit,*,err=20) imeas
-         read(input_unit,*,err=20) imode   
+         read(input_unit,*,err=20) imode
 
          close(input_unit)
-         
-*     
-*     error message 
-*     
+
+*
+*     error message
+*
          go to 30
  20      write(*,*)'break: data input',n_lattice,dimproc,nproc,init,rand,
      &        save,confstart,beta,x,y,lambda,limit_or,inorm,nsweep_flag,ntime,
      &        nsweep,imeas,imode
          call abort()
  30      continue
-         
-         
-*     
+
+
+*
 *     the parameters are given for the user
-*     
+*
          write(6,1000)n_lattice(1), n_lattice(2), n_lattice(3),
      &        dimproc(1), dimproc(2), dimproc(3), nproc
-         write(*,*)'init 0=rand,1=onerand,2=unit,3=old   :', init        
-         write(*,*)'rand 1=old, 0=new random numbers     :', rand        
-         write(*,*)'save on disc yes or no (1,0)         :', save        
-         write(*,*)'start configuration                  :', confstart   
-         write(*,*)'beta                                 :', beta        
+         write(*,*)'init 0=rand,1=onerand,2=unit,3=old   :', init
+         write(*,*)'rand 1=old, 0=new random numbers     :', rand
+         write(*,*)'save on disc yes or no (1,0)         :', save
+         write(*,*)'start configuration                  :', confstart
+         write(*,*)'beta                                 :', beta
          write(*,*)'x                                    :', x
          write(*,*)'y                                    :', y
          write(*,*)'overrelaxation parameter             :', lambda
@@ -130,17 +130,17 @@
          write(*,*)'limit_or                             :', limit_or
          write(*,*)'limit_it parameters                  :', limit_it
          write(*,*)'inorm                                :', inorm
-         write(*,*)'flag (0=nb of sweeps, 1=time in min) :', nsweep_flag  
-         write(*,*)'max time for the program in min      :', ntime   
-         write(*,*)'number of sweeps                     :', nsweep       
-         write(*,*)'imeas                                :', imeas        
-         write(*,*)'heatbath mode                        :', imode        
+         write(*,*)'flag (0=nb of sweeps, 1=time in min) :', nsweep_flag
+         write(*,*)'max time for the program in min      :', ntime
+         write(*,*)'number of sweeps                     :', nsweep
+         write(*,*)'imeas                                :', imeas
+         write(*,*)'heatbath mode                        :', imode
       endif
 *
 *     create new types for the broadcast
-*     
+*
       call paratypeconstruct
-      
+
       CALL MPI_BCAST(beta,1,paramettype,root,MPI_COMM_WORLD,ierror)
       CALL MPI_BCAST(init,1,configtype,root,MPI_COMM_WORLD,ierror)
       CALL MPI_BCAST(n_lattice(1),1,latconftype,root,MPI_COMM_WORLD,
@@ -151,20 +151,20 @@
       beta_2=beta_a*( 3+8*y/beta**2-sigma*(4+5*x)/(2*pi*beta)-
      &       ( (20*x-10*x**2)*(log(1.5*beta)+0.09)+8.7+11.6*x)/(2*pi**2*beta**2) )
       beta_4=x*beta
-          
+
       do i = 0, nproc-1
          if(myrank==i)then
             write(*,*)'********************************************'
             write(*,*)'myrank = ', myrank
             write(*,*)'********************************************'
             write(*,1000) n_lattice(1), n_lattice(2), n_lattice(3),
-     &                  dimproc(1),   dimproc(2),   dimproc(3)   
-            write(*,*)'init 0=rand,1=onerand,2=unit,3=old   :', init        
-            write(*,*)'rand 1=old, 0=new random numbers     :', rand        
-            write(*,*)'save on disc yes or no (1,0)         :', save        
-            write(*,*)'start configuration                  :', confstart   
+     &                  dimproc(1),   dimproc(2),   dimproc(3)
+            write(*,*)'init 0=rand,1=onerand,2=unit,3=old   :', init
+            write(*,*)'rand 1=old, 0=new random numbers     :', rand
+            write(*,*)'save on disc yes or no (1,0)         :', save
+            write(*,*)'start configuration                  :', confstart
 
-            write(*,*)'beta                                 :', beta        
+            write(*,*)'beta                                 :', beta
             write(*,*)'x                                    :', x
             write(*,*)'y                                    :', y
             write(*,*)'overrelaxation parameter             :', lambda
@@ -172,16 +172,16 @@
             write(*,*)'limit_or                             :', limit_or
             write(*,*)'limit_it parameters                  :', limit_it
             write(*,*)'inorm                                :', inorm
-            write(*,*)'flag (0=nb of sweeps, 1=time in min) :', nsweep_flag  
-            write(*,*)'max time for the program in min      :', ntime   
-            write(*,*)'number of sweeps                     :', nsweep       
-            write(*,*)'imeas                                :', imeas        
-            write(*,*)'heatbath mode                        :', imode        
+            write(*,*)'flag (0=nb of sweeps, 1=time in min) :', nsweep_flag
+            write(*,*)'max time for the program in min      :', ntime
+            write(*,*)'number of sweeps                     :', nsweep
+            write(*,*)'imeas                                :', imeas
+            write(*,*)'heatbath mode                        :', imode
 
          end if
          call MPI_BARRIER(comm, ierror)
       end do
-      
+
       return
 
 1000  format('Adjoint Higgs Modell on a '/
@@ -189,33 +189,4 @@
      &     'on a  ',i2,' X ',i2,' X ',i2,' processor lattice'/
      &        'number of processors :',i3/)
 
-      end
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      end 
